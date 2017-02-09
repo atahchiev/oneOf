48b84e2d28752bc555fa281a3b5c85bb548e1f76
Aleksandar Tahchiev <atahchiev@codixfr.private>
2017-02-09 17:56:58 +0200

    milen

--- knn/classInof.py
+++ knn/classInof.py
@@ -1,19 +1,24 @@
-class WordEvaluation:
-    def __init__(self, word_name=None, tf = 0, idf=0, tf_idf=0):
-        self.word_name = word_name
-        self.tf = tf
-        self.idf = idf
-        self.tf_idf = tf*idf
+import math
 
+class WordEvaluation:
+    def __init__(self, word_name=None, word_frequency=0, docs_with_word=0):
+        self.word_name = word_name
+        self.word_frequency = self.calc_tf()
+        self.tf = self.calc_tf()
+        self.idf = self.calc_idf()
+        self.tf_idf = self.tf*self.idf
+        self.docs_with_word = docs_with_word
 
     def calc_tf(self):
-        pass
+        if self.word_frequency == 0:
+            return 0
+        elif self.word_frequency > 0:
+            return 1 + math.log(self.word_frequency,10)
 
-    def calc_idf(self):
-        pass
+    def calc_idf(self, documents):
+        return math.log(documents/self.word_frequency,10)
 
-    # def pee
 
 class ClassInfo:
-    def __init__(self, d=None, tf = 0, idf = 0):
+    def __init__(self, d=None, tf=0, idf=0):
         pass
